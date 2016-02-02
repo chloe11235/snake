3687ad9ccec4dbdbe93338f0fa498cd7bd8eb4a9
Theo lou <Theo lou>
2016-02-02 12:16:03 +0100

    Creation du GameElement V1.O à tester

---
+++ Project-Snake/GameElement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Snake
+{
+    
+    class GameElement : PictureBox
+    {
+        private enum ElementName { SNAKEHEAD, SNAKE, FRUIT, WALL };
+
+        private ElementName Nom;
+
+        private int posX;
+
+        public int Horizontal
+        {
+            get { return posX; }
+            set { posX = value; }
+        }
+
+        private int posY;
+
+        public int Vertical
+        {
+            get { return posY; }
+            set { posY = value; }
+        }
+
+        public GameElement (ElementName type, int x, int y)
+        {
+            Nom = type; 
+            posX = x;
+            posY = y;
+            this.Height = 10;
+            this.Width = 10;
+        }
+       
+    }
+}
+
+
