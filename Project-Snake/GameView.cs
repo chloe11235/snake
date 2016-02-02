addcbe49cd946654ab389e71026e0d28aa88f92a
chloe <chl.chopin@gmail.com>
2016-02-02 13:44:20 +0100

    creation GameView

---
+++ Project-Snake/GameView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Snake
+{
+    class GameView: Panel
+
+    {
+        public   GameView()
+        { 
+           this.Width = 10*scaleDef.ScaleWidth;
+
+            
+        }
+
+        
+
+       
+        
+       
+        
+    }
+
+    class scaleDef
+    {
+        private int scaleHeight=10;
+        private int scaleWidth=10;
+
+        public int ScaleWidth
+        {
+            get { return scaleWidth; }
+            set { scaleWidth = value; }
+        }
+
+        public int ScaleHeight
+        {
+            get { return scaleHeight; }
+            set { scaleHeight = value; }
+        }
+
+              
+
+        
+    }
+}
