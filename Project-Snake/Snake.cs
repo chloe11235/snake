e8fe9e8dab73e4f42f360a3a4edee0612bdcc433
Theo lou <Theo lou>
2016-02-04 15:23:57 +0100

    définition class snake

---
+++ Project-Snake/Snake.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_Snake;
+
+namespace Project_Snake
+{
+    class Snake
+    {
+        private List<GameElement> theSnake = new List<GameElement>();
+
+        public Snake()
+        {
+            theSnake.Add(new GameElement((GameElement.ElementName)0, 112, 182)); // ajoute la tête du serpent
+            theSnake.Add(new GameElement((GameElement.ElementName)1, 122, 192)); // ajoute un 1er bout de corps
+            theSnake.Add(new GameElement((GameElement.ElementName)1, 132, 202)); // ajoute un 2eme bout de corps
+        }
+
+        
+
+    }
+}
